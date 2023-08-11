b7beb150d4b76ddef2c000942c4782b8cbf20a21
Pratik <pratiked7@gmail.com>
2023-08-11 15:52:28 +0530

    implemented user repository

--- src/main/java/com/jadhoc/expensetracker/repositories/UserRepositoryImpl.java
+++ src/main/java/com/jadhoc/expensetracker/repositories/UserRepositoryImpl.java
@@ -2,12 +2,53 @@ package com.jadhoc.expensetracker.repositories;
 
 import com.jadhoc.expensetracker.domain.User;
 import com.jadhoc.expensetracker.exceptions.EtAuthException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+@Repository
 public class UserRepositoryImpl implements UserRepository{
+
+    private static final String SQL_CREATE = "INSERT INTO ET_USERS(USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD) " +
+            "VALUES (NEXTVAL('ET_USERS_SEQ'), ?, ?, ?, ?)";
+    private static final String SQL_COUNT_BY_EMAIL = "SELECT COUNT(*) FROM ET_USERS WHERE EMAIL = ?";
+    private static final String SQL_FIND_BY_ID = "SELECT USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD FROM ET_USERS " +
+            "WHERE USER_ID = ?";
+
+
+    //for DB interaction
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Override
     public Integer create(String firstName, String lastName, String email, String password) throws EtAuthException {
-        return null;
+
+        try {
+
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+            jdbcTemplate.update(connection -> {
+
+                PreparedStatement ps = connection.prepareStatement(SQL_CREATE, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, firstName);
+                ps.setString(2, lastName);
+                ps.setString(3, email);
+                ps.setString(4, password);
+
+                return ps;
+
+            }, keyHolder);
+
+            return (Integer) keyHolder.getKeys().get("USER_ID");
+
+        } catch (Exception e){
+            throw new EtAuthException("Invalid details. Failed to create account");
+        }
     }
 
     @Override
@@ -17,11 +58,19 @@ public class UserRepositoryImpl implements UserRepository{
 
     @Override
     public Integer getCountByEmail(String email) {
-        return null;
+        return jdbcTemplate.queryForObject(SQL_COUNT_BY_EMAIL, Integer.class, email);
     }
 
     @Override
     public User findById(Integer userId) {
-        return null;
+        return jdbcTemplate.queryForObject(SQL_FIND_BY_ID, userRowMapper, userId);
     }
+
+    private RowMapper<User> userRowMapper = ((rs, rowNum) -> {
+        return new User(rs.getInt("USER_ID"),
+                rs.getString("FIRST_NAME"),
+                rs.getString("LAST_NAME"),
+                rs.getString("EMAIL"),
+                rs.getString("PASSWORD"));
+    });
 }
--- src/main/java/com/jadhoc/expensetracker/resources/UserResource.java
+++ src/main/java/com/jadhoc/expensetracker/resources/UserResource.java
@@ -1,25 +1,38 @@
 package com.jadhoc.expensetracker.resources;
 
+import com.jadhoc.expensetracker.domain.User;
+import com.jadhoc.expensetracker.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserResource {
 
+    @Autowired
+    UserService userService;
+
 
     @PostMapping("/register")
-    public String registerUser(@RequestBody Map<String, Object> userMap){
+    public ResponseEntity<Map<String, String>> registerUser(@RequestBody Map<String, Object> userMap){
         String firstName = (String) userMap.get("firstName");
         String lastName = (String) userMap.get("lastName");
         String email = (String) userMap.get("email");
         String password = (String) userMap.get("password");
 
-        return firstName + ", " + lastName + ", " + email + ", " + password;
+        User user = userService.registerUser(firstName, lastName, email, password);
+        Map<String, String> map = new HashMap<>();
+        map.put("Message", "registered successfully : " + user.getUserId());
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
 }
